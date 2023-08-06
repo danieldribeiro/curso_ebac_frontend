d2db5ce37874a451839ef540363f9a84fc29c93b
Daniel Ribeiro <daniel.dribeiro@hotmail.com>
2023-08-06 10:28:24 -0300

    Chore: Finished

--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
 let inputA = document.querySelector('#inputA')
 let inputB = document.querySelector('#inputB')
 let btn = document.querySelector('button')
+let errorMessage = document.querySelector('#error-alert')
+let successMessage = document.querySelector('#success-alert')
 
 btn.addEventListener('click', (e) => {
-    if(inputA.value < inputB.value){
-        let errorMessage = document.querySelector('#error-alert')
-        errorMessage.style.display = 'block'
+    if(inputA.value > inputB.value){
+        messageShow()
+        setTimeout(messageHidden, 3000)
     } else {
-        errorMessage.style.display = 'none'
+        messageShow()
+        setTimeout(messageHidden, 3000)
     }
 
     e.preventDefault()
     inputA.value = ''
     inputB.value = ''
-
 })
+
+function messageShow(){
+    if(inputA.value > inputB.value){
+        errorMessage.style.display = 'block'
+    } else if(inputA.value < inputB.value){
+        successMessage.style.display = 'block'
+    }
+}
+
+function messageHidden(){
+    errorMessage.style.display = 'none'
+    successMessage.style.display = 'none'
+}
