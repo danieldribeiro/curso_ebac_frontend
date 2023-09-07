1f2d0c56d0bb4af889129cf8fd29fb3d7a8ff6bc
danieldribeiro <daniel.dribeiro@hotmail.com>
2023-09-07 10:23:25 -0300

    Finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-const uglify = require('gulp-uglify')
-const sourceMaps = require('gulp-sourcemaps')
-const imageMin = require('gulp-imagemin')
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const sourceMaps = require('gulp-sourcemaps');
+const imageMin = require('gulp-imagemin');
 
-function sassCompiler(){
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sourceMaps.init())
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(sourceMaps.write('./maps'))
-        .pipe(gulp-dest('./build/styles'))
+function sassCompiler() {
+  return gulp.src('./src/styles/*.scss')
+    .pipe(sourceMaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(sourceMaps.write('./maps'))
+    .pipe(gulp.dest('./build/styles'));
 }
 
-function jsCompiler(){
-    return gulp.src('./src/scripts/*.js')
-        .pipe(uglify())
-        .pipe(gulp-dest('./build/scripts'))
+function jsCompiler() {
+  return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'));
 }
 
-function imageCompressor(){
-    return gulp('./src/images/*')
-        .pipe(imageMin())
-        .pipe(gulp-dest('./build/images'))
+function imageCompressor() {
+  return gulp.src('./src/images/*')
+    .pipe(imageMin())
+    .pipe(gulp.dest('./build/images'));
 }
 
-exports.default = function(){
-    gulp.watch('./src/styles/*.scss', {igonoreInitial: false}, gulp.series(sassCompiler));
-    gulp.watch('./src/scripts/*.js', {igonoreInitial: false}, gulp.series(jsCompiler));
-    gulp.watch('./src/images/*', {igonoreInitial: false}, gulp.series(imageCompressor));
-}
+exports.default = function () {
+  gulp.watch('./src/styles/*.scss', { ignoreInitial: false }, gulp.series(sassCompiler));
+  gulp.watch('./src/scripts/*.js', { ignoreInitial: false }, gulp.series(jsCompiler));
+  gulp.watch('./src/images/*', { ignoreInitial: false }, gulp.series(imageCompressor));
+};
