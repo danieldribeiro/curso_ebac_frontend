d227ca699730174b9600d0e0ab740869602d5f76
danieldribeiro <daniel.dribeiro@hotmail.com>
2023-10-04 09:19:39 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplica(valor1, valor2) {
+    return valor1 * valor2;
+}
+function saudacao(nome) {
+    return 'Olá ' + nome;
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+function multiplica(valor1: number, valor2: number){
+    return valor1 * valor2;
+}
+
+function saudacao(nome: string){
+    return 'Olá ' + nome;
+}
+
+saudacao('Daniel')
